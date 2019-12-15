623a8fedfa427af679e3d9564e0b440fcf2e33bf
christopher tate <chris29tate2@hotmail.co.uk>
2019-12-15 00:22:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,21 +15,21 @@ def add_element_to_start_of_array(create_an_empty_array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  #pop = planes
+  #pop = train
 end
 
 def remove_element_from_start_of_array(array)
-  #shift= 
+  #shift = taxi
 end
 
 def retrieve_element_from_index(array, index_number)
-  []
+  [1] = truck
 end
 
 def retrieve_first_element_from_array(array)
-  [0]
+  [1] truck
 end
 
 def retrieve_last_element_from_array(array)
-  [-1]
+  [-1] 
 end
